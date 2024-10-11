ab41f5bdfc6bd946fcb8ff29d73d1405d97ab987
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-10-11 22:21:16 +0700

    Membuat fungsi dengan nama uppercaseString dengan parameter string dengan tipe data String. Dimana pada fungsi ini terdapat return yang mengembalikan nilai parameter string dengan fungsi .uppercase yang akan mengubah nilai menjadi huruf besar.

---
+++ app/src/main/java/com/example/myapplication_test/LambdaFunction.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication_test
+
+// Lambda Functions
+// Lambda Functions adalah fungsi yang tidak memiliki nama
+// Lambda Functions biasanya digunakan untuk membuat fungsi yang sederhana
+// Lambda Functionas menggunakan tanda panah (->) untuk memisahkan parameter dan body fungsi
+
+fun uppercaseString(string: String): String {
+    return string.uppercase()
+}
