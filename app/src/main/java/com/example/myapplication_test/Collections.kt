6dafeb3867fe0bb46279f9dfaff6b77b0f4d4462
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-10-11 20:04:26 +0700

    Membuat variabel di dalam fungsi ContohMap yang berupa map yang bersifat mutable dengan nama variabel shape dengan MutableMap bertipe data String sebagai kunci dan Int sebagai nilai dengan operasi menggunakan mutableMapOf. Kemudian mencetaknya dengan println nama variabel yaitu shape.

--- app/src/main/java/com/example/myapplication_test/Collections.kt
+++ app/src/main/java/com/example/myapplication_test/Collections.kt
@@ -80,6 +80,10 @@ fun ContohMap() {
     val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
     println(readOnlyShape)
 
+    // Map Mutable
+    val shape: MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(shape)
+
 }
 
 
