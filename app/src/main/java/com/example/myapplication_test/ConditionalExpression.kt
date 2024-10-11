ed0427b8f046194d65bdeca1edb24c18889fec62
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-10-11 20:27:36 +0700

    Membuat fungsi dengan nama ConditionalStatement yang akan berisi conditional expression.

---
+++ app/src/main/java/com/example/myapplication_test/ConditionalExpression.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication_test
+
+// Conditional Expression
+// Conditional expression adalah ekpresi yang mengembalikan nilai berdasarkan kondisi tertentu
+// Conditional expression menggunakan if-else, when, dan try-catch
+
+fun ConditionalStatement() {
+    println("=== Conditional Expression ===")
+
+}
