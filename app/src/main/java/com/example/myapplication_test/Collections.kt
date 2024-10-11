e45021144ed82b390f78b8a770ce7760bed966e0
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-10-11 19:57:52 +0700

    Membuat variabel di dalam fungsi ContohMap yang berupa map yang bersifat read only dengan nama variabel readOnlyShape dengan Map bertipe data String sebagai kunci dan Int sebagai nilai dengan operasi menggunakan mapOf. Kemudian mencetaknya dengan println nama variabel yaitu readOnlyShape.

--- app/src/main/java/com/example/myapplication_test/Collections.kt
+++ app/src/main/java/com/example/myapplication_test/Collections.kt
@@ -76,6 +76,10 @@ fun ContohMap() {
 
     println()
     println("=== Map ===")
+    //Map Read-Only
+    val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(readOnlyShape)
+
 }
 
 
