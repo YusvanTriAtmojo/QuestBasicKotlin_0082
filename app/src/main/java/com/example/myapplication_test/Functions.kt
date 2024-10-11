e17e6f208f4d4214e523a1bdb05ce9ca7b7eab44
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-10-11 21:39:31 +0700

    Membuat fungsi dengan nama withoutParameter yaitu fungsi yang tidak memiliki parameter. Lalu, fungsi dengan nama withParameter yaitu fungsi dengan parameter berupa name dengan tipe data string dan mencetak nilai parameter name tersebut.

---
+++ app/src/main/java/com/example/myapplication_test/Functions.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication_test
+
+fun withoutParameter() {
+    println("== withoutParameter ==")
+    println("Hello, World!")
+}
+
+fun withParameter(name: String) {
+    println()
+    println("== withParameter ==")
+    println("Hello, $name!")
+}
